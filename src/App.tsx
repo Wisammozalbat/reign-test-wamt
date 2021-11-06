aea445536eca5ae0f1f568092587c55b7a96eef2
Wisam <wisam_mozalbat@hotmail.com>
2021-11-06 03:43:44 -0400

    moved layout in directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Main from './containers/main/Main';
-import Layout from './layout/Layout';
+import Layout from './containers/layout/Layout';
 
 function App() {
   return (
